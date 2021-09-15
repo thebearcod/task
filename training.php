69d27d4c966876dfeb8df030219b2ed6f67c90d2
Maxim <47496840+thebearcod@users.noreply.github.com>
2021-09-15 09:48:45 +0500

    new training

--- training.php
+++ training.php
@@ -2,6 +2,18 @@
 
 // потренируем регулярки
 $string = "PHP56_testing";
+
+
+$text = "Привет мир!";
+echo strlen($text);
+echo '<br>';
+echo 6 / ($d = (5 % 2) - 1);echo '<br>';
+
+$arr = array(0,1,2);
+foreach ($arr as $value) {
+    $value += 2;
+}
+print_r($arr);
 ?>
 <ul>
 <li>1 => <?print_r(preg_match("/^[A-Za-z0-9]*$/", $string));?></li>
@@ -10,3 +22,4 @@ $string = "PHP56_testing";
 <li>4 => <?print_r(preg_match( "/56_test/", $string));?></li>
 <li>5 => <?print_r(preg_match( "/[0-9]+$/", $string ));?></li>
 </ul>
+
